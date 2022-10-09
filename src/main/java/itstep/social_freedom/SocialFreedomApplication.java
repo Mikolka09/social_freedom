4b5b28e9b0799deeaf0b8d9854573a1c62f46e74
Микола Матіос <mikolka09@gmail.com>
2022-10-09 21:46:25 +0200

    09.10.2022

---
+++ src/main/java/itstep/social_freedom/SocialFreedomApplication.java
@@ -0,0 +1,11 @@
+package com.boots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFreedomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialFreedomApplication.class, args);
+    }
+}
---
+++ src/test/java/itstep/social_freedom/SocialFreedomApplicationTests.java
@@ -0,0 +1,13 @@
+package itstep.social_freedom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFreedomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
