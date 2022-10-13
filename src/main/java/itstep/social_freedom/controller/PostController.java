3a83876371a2a4b57f96ea5388afd711228b8a95
Микола Матіос <mikolka09@gmail.com>
2022-10-13 21:58:54 +0200

    13.10.2022

--- src/main/java/itstep/social_freedom/controller/PostController.java
+++ src/main/java/itstep/social_freedom/controller/PostController.java
@@ -1,4 +1,15 @@
 package itstep.social_freedom.controller;
 
+import itstep.social_freedom.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
 public class PostController {
+
+
 }
